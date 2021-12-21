d16194716babbe24125def134667e16a0b3f7e78
muzhailong <1993414600@qq.com>
2021-12-21 08:58:14 +0800

    fix run param

--- run_distribute.sh
+++ run_distribute.sh
@@ -6,13 +6,13 @@ python3 -m oneflow.distributed.launch \
     --node_rank 1 \
     --master_addr cn16 \
     --master_port 7788 \
-    --node_ips ['cn15','cn16'] \
     run_eager_pretraining.py \
+    --node_ips ['cn15','cn16'] \
     --num_hidden_layers 12 \
     --num_attention_heads 12 \
     --max_position_embeddings 512 \
     --gpu_num_per_node 2 \
-    --grad_acc_steps 2 \
+    --grad-acc-steps 2 \
     --seq_length 128 \
     --vocab_size 30522 \
     --type_vocab_size 2 \
